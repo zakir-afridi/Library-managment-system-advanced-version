3c88efee273121e92129068ae9ca14dce458f603
realusmannazir1 <24pwdsc0178@uetpeshawar.edu.pk>
2026-09-01 18:18:34 +0500

    feat: add Main.java entry point for automatic build and application launching

--- Library-managment-system-advanced-version/Library/Main.java
+++ Library-managment-system-advanced-version/Library/Main.java
@@ -43,7 +43,7 @@ public class Main {
             "-Dsun.misc.unsafe.warn=false",
             "-Dfile.encoding=UTF-8",
             "-cp", classPath,
-            "com.library.Main"
+            "com.library.LibraCoreApp"
         );
     }
 
